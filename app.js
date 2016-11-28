a7ea026364886989becad975d3ba5b64d5ab77b9
Sandeep C <sandyc2992@gmail.com>
2016-11-28 18:18:07 +0530

    basic ui and layout done

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 80;
+var host = require('os').hostname();
+var index = require('./routes/index');
+
+var app = express();
+__dirname = path.resolve();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use('/galleryScripts', express.static(__dirname + '/node_modules/ng-image-gallery/dist/'));
+
+
+if (host == 'ip-35-154-56-54') {
+    app.BASE_URL = 'http://anjanasandeep.com';
+} else {
+    app.BASE_URL = 'http://localhost';
+}
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+console.log(port);
+app.listen(port, 'localhost', function () {
+    console.log('running');
+});
+console.log('The magic happens on port ' + port);
+
+module.exports = app;
